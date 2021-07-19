6053ae79a5d4efcaacdd1fd631c01bdb0e9944bf
Md. Abdullah Al Mahmud <shaphil2025@gmail.com>
2021-07-19 19:57:51 +0600

    Add django crispy forms and bootstrap5 support for it

--- recipe_box/settings.py
+++ recipe_box/settings.py
@@ -37,7 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # plugins
+    'crispy_forms',
+    'crispy_bootstrap5',
+    # user apps
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
