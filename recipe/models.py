c4bd2bcb932e601cef48bc872e1f8b9f36a861cc
Md. Abdullah Al Mahmud <shaphil2025@gmail.com>
2021-07-23 02:05:09 +0600

    Add models Ingredients and Recipe

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from django.db import models
 
 
@@ -31,3 +33,24 @@ class Unit(models.Model):
 
     def __str__(self):
         return self.symbol
+
+
+class Ingredients(models.Model):
+    name = models.CharField(max_length=128, blank=False, null=False)
+    article_number = models.CharField(max_length=16, blank=True, null=True)
+    unit = models.ForeignKey(
+        Unit, null=True, blank=True, on_delete=models.SET_NULL)
+    unit_amount = models.FloatField(blank=True, null=True)
+    price_per_unit_amount = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=256, blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.name[:16]}...'
