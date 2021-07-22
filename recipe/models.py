3846b1814a0c1169b7733f8adcf5fe95b0583972
Md. Abdullah Al Mahmud <shaphil2025@gmail.com>
2021-07-22 20:43:29 +0600

    Add model Unit

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStampMixin:
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+
+class Unit(models.Model, TimeStampMixin):
+    """Represents a metric unit of measurement.
+
+    Base units are:
+        mass -> grams
+        volume -> liter
+    """
+
+    unit_choices = (
+        ('solid', 'Solid'),
+        ('liquid', 'Liquid'),
+        ('solid_and_liquid', 'Solid and Liquid')
+    )
+
+    name = models.CharField(max_length=32)
+    unit_type = models.CharField(max_length=32, choices=unit_choices)
+    multiplier = models.FloatField(null=True)
+    symbol = models.CharField(max_length=16)
+
+    def __str__(self):
+        return self.symbol
