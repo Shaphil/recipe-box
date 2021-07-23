7a9ca9821e0ab5bad52dbc264385506e157d4448
Md. Abdullah Al Mahmud <shaphil2025@gmail.com>
2021-07-23 14:27:28 +0600

    Add view and template for Recipe Details

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import RecipesList
-
+from .views import RecipeDetail, RecipesList
 
 urlpatterns = [
     path('', RecipesList.as_view(), name='recipes-list'),
+    path('<int:pk>/', RecipeDetail.as_view(), name='recipe-detail'),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,9 +1,31 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
-from .models import Recipe
+from .models import Recipe, RecipeIngredients
+
+from decimal import Decimal
 
 
 class RecipesList(ListView):
     """Lists all recipes"""
 
     queryset = Recipe.objects.all()
+
+
+class RecipeDetail(DetailView):
+
+    model = Recipe
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        recipe = context.get('recipe')
+        recipe_ingredients = RecipeIngredients.objects.filter(
+            recipe_id=recipe.id)
+
+        total_price = Decimal('0.0')
+        for ingredient in recipe_ingredients:
+            total_price += round(ingredient.price, 2)
+
+        context['ingredients'] = recipe_ingredients
+        context['total_price'] = round(total_price, 2)
+
+        return context
